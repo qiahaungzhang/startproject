31c7c737807fd017b1425370e7fc7ff1b9b59de6
qiahuangzhang <2293477745@qq.com>
2017-08-01 22:20:13 +0800

    这是我第一提交。

--- src/com/ivyb2b/TestProject.java
+++ src/com/ivyb2b/TestProject.java
@@ -2,6 +2,7 @@ package com.ivyb2b;
 
 public class TestProject {
     public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World!shshdhfhfh");
+		System.out.println("第二次计较");
 	}
 }
